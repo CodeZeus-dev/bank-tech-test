50fb880bdb075f2b3ef5163ae7d701411501226f
CodeZeus-dev <constantine.akritides@gmail.com>
2021-01-11 18:16:58 +0200

    written tests for tracking the balance, and for depositing an amout

--- spec/unit/BankTechSpec.js
+++ spec/unit/BankTechSpec.js
@@ -2,18 +2,32 @@ const BankTech = require('../../lib/BankTech');
 
 describe('BankTech', function() {
     let bankTech;
+    const DEPOSIT_AMOUNT = 500;
 
     beforeEach(function() {
         bankTech = new BankTech();
     })
 
-    it('creates a balance upon initialisation', function() {
-        expect(bankTech.balance).toEqual(0);
-    });
+    describe("Tracking Balance", function() {
+        it('creates a balance upon initialisation', function() {
+            expect(bankTech.balance).toEqual(0);
+        });
 
-    // describe("Deposit Functionality", function() {
-    //     it('enables a user to deposit money to account', function() {
-        
-    //     })
-    // })
+        it('returns the current balance when currentBalance is called', function() {
+            expect(bankTech.currentBalance()).toBeDefined();
+            expect(bankTech.currentBalance()).toEqual(0);
+        });
+    })
+    
+    describe("Deposit Functionality", function() {
+        it('uses the deposit function to deposit funds into the account', function() {
+            expect(bankTech.deposit).toBeDefined();
+        });
+
+        it('enables a user to deposit money to account', function() {
+            bankTech.deposit(DEPOSIT_AMOUNT);
+            expect(bankTech.currentBalance()).toEqual(DEPOSIT_AMOUNT);
+        });
+
+    })
 })
